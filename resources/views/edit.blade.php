43b2b50ed8126c52272475ebeef0f9255daf684e
feoktistovartyom <aoddi@yandex.ru>
2024-08-22 14:07:26 +0500

    added edit form and route update

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title', 'Редактирование задачи')
+
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: .8rem;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <form action="{{ route('tasks.update', ['id' => $task->id]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="">
+            <label for="title">Заголовок</label>
+            <input type="text" name="title" id="title" value="{{ $task->title }} ">
+        </div>
+        @error('title')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
+        <div class="">
+            <label for="description">Описание</label>
+            <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
+        </div>
+        @error('description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
+        <div class="">
+            <label for="long_description">Длинное Описание</label>
+            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
+        </div>
+        @error('long_description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
+
+        <div class="">
+            <button type="submit">Редактировать задачу</button>
+        </div>
+    </form>
+@endsection
