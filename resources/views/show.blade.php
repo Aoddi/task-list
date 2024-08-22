0896fb0cbdf764e8a4870bdb8f90c90b9c46d034
feoktistovartyom <aoddi@yandex.ru>
2024-08-22 17:39:23 +0500

    added the ability to delete tasks

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,4 +11,12 @@
 
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+    <div class="">
+        <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Удалить</button>
+        </form>
+    </div>
 @endsection
