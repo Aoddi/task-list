1bacc5f9f15f8c2a5cae3fd417f6ee8e77c484c8
feoktistovartyom <aoddi@yandex.ru>
2024-08-22 13:45:41 +0500

    added session message and error message

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,24 +2,41 @@
 
 @section('title', 'Новая задача')
 
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: .8rem;
+        }
+    </style>
+@endsection
+
 @section('content')
-    {{ $errors }}
     <form action="{{ route('tasks.store') }}" method="POST">
         @csrf
         <div class="">
             <label for="title">Заголовок</label>
             <input type="text" name="title" id="title">
         </div>
+        @error('title')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
 
         <div class="">
             <label for="description">Описание</label>
             <textarea name="description" id="description" rows="5"></textarea>
         </div>
+        @error('description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
 
         <div class="">
             <label for="long_description">Длинное Описание</label>
             <textarea name="long_description" id="long_description" rows="10"></textarea>
         </div>
+        @error('long_description')
+        <p class="error-message">{{ $message }}</p>
+        @enderror
 
         <div class="">
             <button type="submit">Создать задачу</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Task List App</title>
+    @yield('styles')
 </head>
 <body>
     <h1>@yield('title')</h1>
 
     <div class="">
+        @if(session()->has('success'))
+            <div class="">{{ session('success') }}</div>
+        @endif
+
         @yield('content')
     </div>
 </body>
