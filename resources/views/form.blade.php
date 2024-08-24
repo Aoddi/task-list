daca8a849f60ac3a3af0ec03c030e1beb1224573
feoktistovartyom <aoddi@yandex.ru>
2024-08-24 19:27:21 +0500

    add styles forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,53 +2,54 @@
 
 @section('title', isset($task) ? 'Редактирование задачи' : 'Новая задача')
 
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: .8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-    <form action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}" method="POST">
+    <form action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}"
+          method="POST">
         @csrf
         @isset($task)
             @method('PUT')
         @endisset
-        <div class="">
+        <div class="mb-4">
             <label for="title">Заголовок</label>
-            <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}">
-        </div>
-        @error('title')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
+            <input type="text" name="title" id="title"
+                   @class(['border-red-500' => $errors->has('title')])
+                   value="{{ $task->title ?? old('title') }}">
 
-        <div class="">
+            @error('title')
+            <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-4">
             <label for="description">Описание</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
-        </div>
-        @error('description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
+            <textarea name="description" id="description" rows="5"
+            @class(['border-red-500' => $errors->has('description')])>{{ $task->description ?? old('description') }}</textarea>
 
-        <div class="">
+            @error('description')
+            <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-4">
             <label for="long_description">Длинное Описание</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
-        </div>
-        @error('long_description')
-        <p class="error-message">{{ $message }}</p>
-        @enderror
+            <textarea name="long_description" id="long_description" rows="10"
+            @class(['border-red-500' => $errors->has('long_description')])>{{ $task->long_description ?? old('long_description') }}</textarea>
 
-        <div class="">
-            <button type="submit">
+            @error('long_description')
+            <p class="error">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="flex items-center gap-2">
+            <button type="submit" class="btn">
                 @isset($task)
                     Изменить задачу
                 @else
                     Создать задачу
                 @endisset
             </button>
+
+            <a href="{{ route('tasks.index') }}" class="link">Назад</a>
         </div>
     </form>
 @endsection
